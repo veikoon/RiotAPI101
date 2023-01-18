ddf646ab8d77fc2f064d16c72a78acb84a774a65
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2023-01-18 17:21:04 +0100

    new stats

--- lol/views.py
+++ lol/views.py
@@ -98,7 +98,64 @@ def LastGameStats(request):
             for challenges_stat in summoner_info["challenges"]:
                 challenges[challenges_stat] = summoner_info["challenges"][challenges_stat]
 
-    data = {"stats": stats, "challenges": challenges}
+    # Vision
+    pings = {}
+    champion = {}
+    visions = {}
+    spells = {}
+    try:
+        pings["Basic"] = [stats["basicPings"], "img/ping/generic.png"]
+        pings["Danger"] = [stats["dangerPings"], "img/ping/danger.png"]
+        pings["On My Way"] = [stats["onMyWayPings"], "img/ping/onmyway.png"]
+        pings["Missing"] = [stats["enemyMissingPings"], "img/ping/missing.png"]
+        pings["Assist me"] = [stats["assistMePings"], "img/ping/assistme.png"]
+        pings["All In"] = [stats["allInPings"], "img/ping/allin.png"]
+        pings["Push"] = [stats["pushPings"], "img/ping/push.png"]
+        pings["Bait"] = [stats["baitPings"], "img/ping/bait.png"]
+        pings["Caution"] = [stats["getBackPings"], "img/ping/caution.png"]
+        pings["Push"] = [stats["pushPings"], "img/ping/push.png"]
+        pings["Hold"] = [stats["holdPings"], "img/ping/hold.png"]
+        pings["Command"] = [stats["commandPings"], "img/ping/command.jpg"]
+        pings["Need Vision"] = [stats["needVisionPings"], "img/ping/needvision.png"]
+        pings["Ennemy Vision"] = [stats["enemyVisionPings"], "img/ping/ennemyvision.png"]
+        pings["Cleared Vision"] = [stats["visionClearedPings"], "img/ping/visioncleared.png"]
+
+        visions["Wards Detected"] = [stats["detectorWardsPlaced"], "img/ping/wardsdetected.png"]
+        visions["Wards Placed"] = [stats["wardsPlaced"], "assets/dragontail/13.1.1/img/item/3340.png"]
+        visions["Wards Destroyed"] = [stats["wardsKilled"], "assets/dragontail/13.1.1/img/item/3364.png"]
+        visions["Control Wards Purchased"] = [challenges["controlWardsPlaced"], "assets/dragontail/13.1.1/img/item/2055.png"]
+
+        champion["champExperience"] = stats["champExperience"]
+        champion["champLevel"] = stats["champLevel"]
+        champion["championName"] = stats["championName"]
+        champion["kills"] = stats["kills"]
+        champion["assists"] = stats["assists"]
+        champion["deaths"] = stats["deaths"]
+
+        f = open('static/assets/dragontail/13.1.1/data/fr_FR/champion/' + stats["championName"] + '.json')
+        champion_data = json.load(f)
+        spell1 = champion_data["data"][stats["championName"]]["spells"][0]["name"]
+        spell2 = champion_data["data"][stats["championName"]]["spells"][1]["name"]
+        spell3 = champion_data["data"][stats["championName"]]["spells"][2]["name"]
+        spell4 = champion_data["data"][stats["championName"]]["spells"][3]["name"]
+
+        spells[spell1] = [stats["spell1Casts"], "assets/dragontail/13.1.1/img/spell/" + stats["championName"]  + "Q.png"]
+        spells[spell2] = [stats["spell2Casts"], "assets/dragontail/13.1.1/img/spell/" + stats["championName"]  + "W.png"]
+        spells[spell3] = [stats["spell3Casts"], "assets/dragontail/13.1.1/img/spell/" + stats["championName"]  + "E.png"]
+        spells[spell4] = [stats["spell4Casts"], "assets/dragontail/13.1.1/img/spell/" + stats["championName"]  + "R.png"]
+        champion["spells"] = spells
+
+
+    except ValueError:
+        messages.error(request, "Erreur interne : " + ValueError)
+
+    data = {
+        "stats": stats,
+        "challenges": challenges,
+        "pings": pings,
+        "visions": visions,
+        "champion": champion,
+        }
     return render(request, template_name, data)
 
         
